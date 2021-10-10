3fe62df1acbed7efe2c6e95762798de7c6f9c460
alexander930828 <77578710+alexander930828@users.noreply.github.com>
2021-10-10 12:23:23 +0900

    todayt

---
+++ zoo.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n+1)
+
+dp[1] = 3
+dp[2] = 7
+dp[3] = 17
+
+for i in range(4, n+1):
+    dp[i] = dp[i-2] + 2 * dp[i-1]
+
+print(dp[n] % 9901)
+
+# import sys
+# input = sys.stdin.readline
+# n = int(input())
+# dp = [0]*(n+1)
+# for i in range(n+1) :
+#     dp[i] = [0,0,0]
+# dp[1][0] = 1
+# dp[1][1] = 1
+# dp[1][2] = 1
+
+# for i in range(2, n + 1):
+#     dp[i][0] = dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]
+#     dp[i][1] = dp[i - 1][0] + dp[i - 1][2]
+#     dp[i][2] = dp[i - 1][0] + dp[i - 1][1]
+
+# print(sum(dp[n]) % 9901)
